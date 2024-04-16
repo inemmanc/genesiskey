db22f67a9e41e8080b5417e0c4f7c67835d89564
Felipe <nemmanc@protonmail.com>
2024-04-16 19:33:47 -0300

    add: selected_method-swtich

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -5,10 +5,24 @@ import (
 	"fmt"
 	"genesiskey/src/text"
 	"os"
+	"strings"
 )
 
 func SelectMethod() {
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Println(text.DefaultMethodText())
 	scanner.Scan()
+
+	selected := strings.ToLower(scanner.Text())
+	switch selected {
+	case "std":
+		// TEMP
+		fmt.Println("DEFAULT STD")
+	case "url":
+		// TEMP
+		fmt.Println("DEFAULT URL")
+	default:
+		// TEMP
+		fmt.Println("DEFAULT STD init")
+	}
 }
