f251bb7661fe6eb59285c62e27f311fad904d003
Felipe <nemmanc@protonmail.com>
2024-04-16 19:15:07 -0300

    add: DefaultWelcomeMessage_func

--- src/text/text.go
+++ src/text/text.go
@@ -1,5 +1,10 @@
 package text
 
+func DefaultWelcomeMessage() string {
+	welcomeMessage := "---- GENESIS KEY (cli) ----"
+	return welcomeMessage
+}
+
 func DefaultMethodText() string {
 	selectText := "Insert the Encoding Method:\nSTD - standard base64 encoding\nURL - used in URLs and file names"
 	return selectText
